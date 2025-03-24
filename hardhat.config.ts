cffec6bcfdbdc63d76655ff5d999ba2c4fa12c20
ogazboiz <ogazboizakpolo@gmail.com>
2025-03-24 14:42:00 +0100

    Done with the contract

---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
+require('dotenv').config()
+
+const {ALCHEMY_SEPOLIA_API_URL, LISK_SEPOLIA_RPC_URL,ACCOUNT_PRIVATE_KEY} = process.env
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+
+  networks: {
+    sepolia: {
+      url: ALCHEMY_SEPOLIA_API_URL,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`] 
+    },
+    lisk_sepolia: {
+      url: LISK_SEPOLIA_RPC_URL,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+export default config;
